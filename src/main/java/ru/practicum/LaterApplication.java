b113cbeef7f20dcde2a9ebbca5899934ec5a73e9
aleksandr <dev.golovkoay@gmail.com>
2024-05-21 12:30:58 +0300

    Добавляем Spring Web: настраиваем ApplicationContext и DispatcherServlet

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -4,23 +4,29 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class LaterApplication {
-    private static final int PORT = 8080;
 
     public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
-        tomcat.getConnector().setPort(PORT);
+        tomcat.getConnector().setPort(8080);
 
-        // то самое «приложение» или «контекст» с пустым путём
         Context tomcatContext = tomcat.addContext("", null);
 
-        // класс Wrapper позволяет задать дополнительные настройки для сервлета
-        Wrapper testServletWrapper =
-                Tomcat.addServlet(tomcatContext, "testServlet", new TestServlet());
+        AnnotationConfigWebApplicationContext applicationContext =
+                new AnnotationConfigWebApplicationContext();
+        applicationContext.scan("ru.practicum");
+        applicationContext.setServletContext(tomcatContext.getServletContext());
+        applicationContext.refresh();
 
-        // addMapping() сопоставляет URL-путь с сервлетом
-        testServletWrapper.addMapping("/test");
+        // добавляем диспетчер запросов
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        Wrapper dispatcherWrapper =
+                Tomcat.addServlet(tomcatContext, "dispatcher", dispatcherServlet);
+        dispatcherWrapper.addMapping("/");
+        dispatcherWrapper.setLoadOnStartup(1);
 
         tomcat.start();
     }
