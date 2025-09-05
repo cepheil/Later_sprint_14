d3c168fb219f628e05a8f1f70be3cc96d2527c45
Sergei Levashov <cephei.l@yandex.ru>
2025-09-05 13:06:55 +0300

    доработки внешнего логирования

--- src/main/java/ru/practicum/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/user/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.practicum.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -13,11 +16,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUsers() {
+        log.info("Попытка получения пользователей");
         return repository.findAll();
     }
 
     @Override
     public User saveUser(User user) {
+        log.info("Попытка создания пользователя {}", user.getName());
         return repository.save(user);
     }
 }
